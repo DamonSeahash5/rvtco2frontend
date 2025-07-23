87cea807e0834a0efa1679f9abd0758a80e2b7c9
DamonSeahash5 <oliverhopwood5@gmail.com>
2025-07-24 00:09:20 +0100

    created animated loading widget

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import { UploadButton } from './buttons/uploadbutton'
 import { UploadWidget } from './widgets/uploadwidget'
+import { LoadingWidget } from './widgets/loading'
 // import { PageBody } from './containers/body'
 import "/src/assets/artem-balashevsky-VkirwC2YH50-unsplash.jpg"
 
@@ -13,6 +14,9 @@ function App() {
   //State to confirm the upload.
   const [uploadConfirmed, setUploadConfirmed] = useState(false)
 
+  //State for the loading / processing animation
+  const [loadingActive, setLoadingActive] = useState(false)
+
   //Sets uploadActive state to true when upload button clicked
   //and toggle to false when Close is clicked
   const handleClick = () => {
@@ -23,18 +27,26 @@ function App() {
   // sets uploadConfirmed to true when confirm upload button is clicked
   const handleConfirm = () => {
     setUploadConfirmed(true);
+    setLoadingActive(true);
     // setUploadActive(false);
     console.log(`Upload confirmed = ${uploadConfirmed}`)
   };
+
+  // const handleLoading = () => {
+  //   uploadActive && uploadConfirmed ? setLoadingActive(true) : setLoadingActive(false)
+  //   //when processed data, add additional state
+  // };
 
   return (
     <>
       <div className="min-h-screen bg-cover bg-center bg-[url('/src/assets/artem-balashevsky-VkirwC2YH50-unsplash.jpg')]">
         <UploadButton uploadActive={uploadActive} onClick={handleClick} />
         <UploadWidget uploadActive={uploadActive} onClick={handleClick} uploadConfirmed={uploadConfirmed} onConfirm={handleConfirm} />
+        {/* <LoadingWidget loadingActive={loadingActive} onLoaded={handleLoading} /> */}
+        <LoadingWidget loadingActive={loadingActive} />
       </ div>
     </>
   )
-}
+};
 
 export default App
--- src/widgets/loading.tsx
+++ src/widgets/loading.tsx
@@ -2,3 +2,20 @@
 // I will mock this up with a timer
 // results from mock JSON data will display after the animation.
 // Use an Icon and apply a rotation animation to it
+// visible whilst loadingState is true
+interface LoadingAnimationProps {
+    loadingActive: boolean;
+    // onLoaded: () => void;
+}
+
+export const LoadingWidget: React.FC<LoadingAnimationProps> = ({ loadingActive }) => {
+    return (
+        <>
+            <div className={loadingActive ? "visible flex h-full justify-center items-center text-purple-700 font-bold" : "invisible"}>
+                <button className="flex-col w-60 h-60 bg-neutral-100/25 animate-spin rounded-full justify-items-center align-items-center text-magenta-500 shadow-lg">
+                    {/* <ArrowUpOnSquareIcon className="w-2/3 stroke-green-700" /> */}
+                    Loading...</button>
+            </div>
+        </>
+    )
+}
