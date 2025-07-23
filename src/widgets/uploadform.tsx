8aee622f62ddf225b7f2f6f433c5c1b49b107780
DamonSeahash5 <oliverhopwood5@gmail.com>
2025-07-23 12:21:11 +0100

    implemented file upload input with explanation & TS error debugging assistance from Copilot

---
+++ src/widgets/uploadform.tsx
@@ -0,0 +1,32 @@
+//A child of upload widget to handle file uploading
+//Create a form with an inut type of file
+//Need to store the file / data in memory somewhere (until API is built)
+//useState of the selected file - lift up to parent
+//onFileChange function to handle upload of the new file and change the state
+
+interface FileUploadProps {
+    currentFile: File | null
+    onFileChange: (file: File | null) => void;
+};
+
+export const FileUpload: React.FC<FileUploadProps> = ({ onFileChange, currentFile }) => {
+
+    return (
+        <>
+            <div>
+                Select File...
+                {/* <input type="file" onChange={e => { onFileChange(e.target.files[0]) }}></input> */}
+                <input
+                    type="file"
+                    onChange={e => {
+                        const file = e.target.files ? e.target.files[0] : null;
+                        onFileChange(file);
+                    }}
+                />
+            </div>
+        </>
+    )
+};
+
+//e.target.files[0] is the selected file object (first of the files list in the inut window)
+//if not selected this could be null, so need to also accept a null input or type
--- src/widgets/uploadwidget.tsx
+++ src/widgets/uploadwidget.tsx
@@ -1,17 +1,31 @@
 // upload widget visible whilst uploadActive = true
-// import { useState } from "react";
-//pass onCLick through to Close button
+//pass onClick through to Close button
+import { useState } from "react";
+import { FileUpload } from "./uploadform";
 
 interface UploadWidgetProps {
     uploadActive: boolean;
     onClick: () => void;
 }
 
-export const UploadWidget: React.FC<UploadWidgetProps> = ({ onClick, uploadActive }) => {
+export const UploadWidget: React.FC<UploadWidgetProps> = ({ onClick, uploadActive, }) => {
+    //Set state of current file to be used for saving / exporting file data
+    const [currentFile, setCurrentFile] = useState<File | null>(null);
+
+    //log the current file on file change
+    const handleFileChange = (currentFile: File | null) => {
+        setCurrentFile(currentFile)
+        //allow for a null value
+        if (currentFile) {
+            console.log(currentFile.name);
+        }
+    }
+
     return (
         <>
             <div className={uploadActive ? "visible relative w-120 h-120 bg-neutral-100/25 rounded-md justify-self-center align-items-center text-magenta-500 shadow-lg" : "invisible"}>
                 Upload Widget
+                <FileUpload currentFile={currentFile} onFileChange={handleFileChange} />
                 <button onClick={onClick} className="absolute hover:bg-red-600/50 top-2 right-2 p-2 rounded-md"> X </button>
             </div>
         </>
